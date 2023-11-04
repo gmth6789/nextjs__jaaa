01c33fddad4c8a1271d1f3930b9e451eead0cc2a
gmth <143313748+gmth6789@users.noreply.github.com>
2023-11-04 10:19:34 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
-module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: [process.env.DOMAIN_URL.replace("https://", "")],
-    loader: "imgix",
-    path: "",
-  },
-  env: {
-    LIFF_ID: process.env.LIFF_ID,
-    LIFF_URL: process.env.LIFF_URL,
-    DOMAIN_URL: process.env.DOMAIN_URL,
-  },
-};
+{
+  "scripts": {
+    "lint": "next lint"
+  }
+}
